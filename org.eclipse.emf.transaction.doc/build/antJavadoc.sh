c10bd2da1c999af1eac8bf4c6d1a6bf2a03e01ed
cmcgee <cmcgee>
2006-03-06 21:04:59 +0000

    [127509] emft_head cmcgee 060306 No Javadocs for EMFT OCL, Query, Transaction, Validation

--- org.eclipse.emf.transaction.doc/build/antJavadoc.sh
+++ org.eclipse.emf.transaction.doc/build/antJavadoc.sh
@@ -1,44 +1,51 @@
+#!/bin/sh
 # The current direcotry
-currentPath=$PWD
+currentPath=`echo "$PWD/$0" | sed -e 's/\(.*\)\/.*/\1\//' | sed -e 's/^[^\/]*$//g'`
 
 # The eclipse directory
 eclipseDir=$1
 
 # The destination directory
-destDir=$eclipseDir/plugins/org.eclipse.emf.transaction.doc/references/javadoc
+destDir=$currentPath/../references/javadoc
+
+mkdir -p $destDir
+
+# The plugin name
+pluginName="org.eclipse.emf.transaction"
 
 # Don't execute if the destination directory has files
-if [ -d "$destDir" ]; then
-	exit
-fi
+#if [ -d "$destDir" ]; then
+#	exit
+#fi
 
 function groupPackage
 {
 	plugin=$1
-	hasToken=`grep "@$plugin@" $currentPath/javadoc.xml.template`
+	hasToken=`grep "@plugin@" $currentPath/javadoc.xml.template`
 	if [ "x$hasToken" != "x"  ]; then
-		srcDir=$eclipseDir/plugins/$plugin/src
+		srcDir=$currentPath/../../$plugin/src
 		if [ -d "$srcDir" ]; then
-			packages=`find $srcDir -type f -name '*.java' -exec grep -e '^package .*;' {} \; | sed -e 's/^package *\(.*\);/\1/' | sort | uniq | xargs | sed -e 's/ /:/g'`
+			packages=`find $srcDir -type f -name '*.java' -exec grep -e '^package .*;' {} \; | sed -e 's/^package *\(.*\);/\1/' | sed -e 's/[ ]*//g' | sort | uniq | xargs | sed -e 's/ /:/g'`
 			packages=`echo $packages | sed -e 's/\//\\\\\\//g' | sed -e 's/\./\\\\\./g'`
-		
-			sed -e "s/\@${plugin}\@/${packages}/g" $currentPath/javadoc.xml.template > javadoc.xml.template.tmp
 	
-			mv javadoc.xml.template.tmp javadoc.xml.template
+			sed -e "s/\@plugin\@/${packages}/g" $currentPath/javadoc.xml.template > $currentPath/javadoc.xml.template.tmp
 		fi
 	fi
 }
 
-groupPackage org.eclipse.emf.transaction
+groupPackage $pluginName
 
-# The directory of the emf.transaction plugins in the order they were built 
-pluginDirs=`find $eclipseDir/plugins -name @dot -printf '%T@ %p\n' | sort -n | grep org.eclipse.emf.transaction | cut -f2 -d' ' | sed -e 's/\(\/.*\)\/.*/\1/'`
+# The directory of the plugins in the order they were built 
+# Original: pluginDirs=`find $eclipseDir/plugins -name @dot -printf '%T@ %p\n' | sort -n | grep org.eclipse.emf.transaction | cut -f2 -d' ' | sed -e 's/\(\/.*\)\/.*/\1/'`
+# New (eclipseDir): pluginDirs=`find $eclipseDir/plugins -name 'org.eclipse.emf.transaction*' -maxdepth 1 -type d -printf '%T@ %p\n' | sort -n | cut -f2 -d' '`
+# Finds plugins in the Workspace:
+pluginDirs=`find $currentPath/../.. -name "${pluginName}*" -maxdepth 1 -type d -printf '%T@ %p\n' | sort -n | cut -f2 -d' '`
 
 ### TODO: missing emf/sdo/xsd plugins (?) in $eclipseDir - need to copy them over or reference source (?)
 ### so that all classes/packages (and thus @links) can be resolved
 
 # All the jars in the plugins directory
-classpath=`find $eclipseDir/plugins -name "*.jar" | tr '\n' ':'`; echo "Got classpath: "; echo $classpath;
+classpath=`find $eclipseDir/plugins -name "*.jar" -printf "%p:"`
 
 # Calculates the packagesets and the calls to copyDocFiles
 packagesets=""
@@ -52,16 +59,33 @@ for pluginDir in $pluginDirs; do
 	fi
 done
 
+# Finds the proper org.eclipse.platform.doc.isv jar
+docjar=`find $eclipseDir/plugins/ -name "org.eclipse.platform.doc.isv*.jar" -printf "%f"`
+
+if [ -f $currentPath/javadoc.xml.template ]; then
+	true;
+else
+	cp $currentPath/javadoc.xml.template $currentPath/javadoc.xml.template.tmp;
+fi
+
 # Replaces the token @packagesets@ in the template by the actual value
 packagesets=`echo $packagesets | sed -e 's/\//\\\\\\//g' | sed -e 's/\./\\\\\./g'`
-sed -e "s/\@packagesets\@/${packagesets}/g" $currentPath/javadoc.xml.template > javadoc.xml.template2
+sed -e "s/\@packagesets\@/${packagesets}/g" $currentPath/javadoc.xml.template.tmp > $currentPath/javadoc.xml.template.tmp2
 # Replaces the token @copydocfiles@ in the template by the actual value
 copydocfiles=`echo $copydocfiles | sed -e 's/\//\\\\\\//g' | sed -e 's/\./\\\\\./g'`
-sed -e "s/\@copydocfiles\@/${copydocfiles}/g" $currentPath/javadoc.xml.template2 > javadoc.xml
+sed -e "s/\@copydocfiles\@/${copydocfiles}/g" $currentPath/javadoc.xml.template.tmp2 > $currentPath/javadoc.xml.template.tmp
+# Replaces the token @docjar@ in the template by the actual value
+sed -e "s/\@docjar\@/${docjar}/g" $currentPath/javadoc.xml.template.tmp > $currentPath/javadoc.xml.template.tmp2
+# Replaces the token @eclipseDir@ in the template by the actual value
+eclipseDirEsc=`echo $eclipseDir | sed -e 's/\//\\\\\//g' | sed -e 's/\./\\\\\./g'`
+sed -e "s/\@eclipseDir\@/${eclipseDirEsc}/g" $currentPath/javadoc.xml.template.tmp2 > $currentPath/javadoc.xml
 
 # Executes the ant script
-ant	-f javadoc.xml \
+ant -f $currentPath/javadoc.xml \
 	-DdestDir="$destDir" \
 	-Dclasspath="$classpath" \
 	-DeclipseDir="$eclipseDir" \
-	-Doverview="$eclipseDir/plugins/org.eclipse.emf.transaction.doc/build/overview.html"
+	-Doverview="$currentPath/overview.html"
+
+# Clean up templates
+rm -f $currentPath/javadoc.xml.template.tmp $currentPath/javadoc.xml.template.tmp2 $currentPath/javadoc.xml
