ef9078faadc8f54aeb7efcdb07c04c16c7cca4a6
Anthony Hunter <anthonyh@ca.ibm.com>
2016-05-18 22:56:06 -0400

    Bug 493963 Fix two failing tests in the Hudson build

--- org.eclipse.emf.transaction.tests/src/org/eclipse/emf/transaction/tests/AbstractTest.java
+++ org.eclipse.emf.transaction.tests/src/org/eclipse/emf/transaction/tests/AbstractTest.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2016 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -472,6 +472,7 @@ public class AbstractTest
 			super();
 		}
 		
+		@SuppressWarnings("unused")
 		public Object caseAudoVisualItem(AudioVisualItem object) {
 			return object.getTitle();
 		}
@@ -698,5 +699,11 @@ public class AbstractTest
 		idle(2000);
 		
 		System.gc();
+	}
+	
+	public void test_DoNothing() {
+		// see Bugzilla 493963
+		String why = "Maven wants to find a test to run in this abstract class";
+		assertTrue(why.contains("Maven"));
 	}
 }
