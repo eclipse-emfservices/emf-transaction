eclipse-emfservices/emf-transaction

commit e0ab1c36a4177595a544da93efc87d5ff0177fc6

[137503] emft_head cdamus 060421 TransactionChangeRecorder resolves containment proxies

org.eclipse.emf.transaction/src/org/eclipse/emf/transaction/impl/TransactionChangeRecorder.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: TransactionChangeRecorder.java,v 1.1 2006/01/30 19:47:55 cdamus Exp $
+ * $Id: TransactionChangeRecorder.java,v 1.2 2006/04/21 14:59:10 cdamus Exp $
  */
 package org.eclipse.emf.transaction.impl;
 
@@ -34,6 +34,7 @@ import org.eclipse.emf.transaction.internal.EMFTransactionPlugin;
 import org.eclipse.emf.transaction.internal.EMFTransactionStatusCodes;
 import org.eclipse.emf.transaction.internal.Tracing;
 import org.eclipse.emf.transaction.internal.l10n.Messages;
+import org.eclipse.emf.transaction.util.TransactionUtil;
 
 /**
  * The change recorder for a {@link org.eclipse.emf.transaction.TransactionalEditingDomain},
@@ -114,12 +115,18 @@ public class TransactionChangeRecorder
 	 * </ul>
 	 */
 	public void setTarget(Notifier target) {
-		// TODO: Account for containment proxies
-		Collection contents = (target instanceof EObject)? ((EObject) target).eContents()
-			: (target instanceof ResourceSet)? ((ResourceSet) target).getResources()
-				: (target instanceof Resource)? ((Resource) target).getContents()
-					: null;
-
+		Collection contents;
+		
+		if (target instanceof EObject) {
+			contents = TransactionUtil.getProperContents((EObject) target);
+		} else if (target instanceof ResourceSet) {
+			contents = ((ResourceSet) target).getResources();
+		} else if (target instanceof Resource) {
+			contents = ((Resource) target).getContents();
+		} else {
+			contents = null;
+		}
+		
 		if (contents != null) {
 			for (Iterator i = contents.iterator(); i.hasNext();) {
 				Notifier notifier = (Notifier) i.next();
@@ -136,7 +143,42 @@ public class TransactionChangeRecorder
 	 * transaction (if any).
 	 */
 	public void notifyChanged(Notification notification) {
-		super.notifyChanged(notification);
+		boolean record = true;
+		
+		switch (notification.getEventType()) {
+		case Notification.SET:
+		case Notification.UNSET:
+		case Notification.ADD:
+		case Notification.ADD_MANY:
+		case Notification.REMOVE:
+		case Notification.REMOVE_MANY:
+			Resource sourceRes = null;
+			Object notifier = notification.getNotifier();
+			
+			if (notifier instanceof Resource) {
+				sourceRes = (Resource) notifier;
+			} else if (notifier instanceof EObject) {
+				sourceRes = ((EObject) notifier).eResource();
+			}
+			
+			if ((sourceRes != null) && !ResourceSetManager.getInstance().isLoaded(sourceRes)) {
+				// resource load and unload are not undoable changes
+				record = false;
+			}
+			
+			break;
+		}
+		
+		if (record || !isRecording()) {
+			super.notifyChanged(notification);
+		} else {
+			try {
+				recording = false;
+				super.notifyChanged(notification);
+			} finally {
+				recording = true;
+			}
+		}
 		
 		if (notification.getEventType() != Notification.REMOVING_ADAPTER) {
 			// the removing adapter event is only sent to me when I am removed
@@ -147,7 +189,7 @@ public class TransactionChangeRecorder
 				processResourceSetNotification(notification);
 			} else if (notifier instanceof Resource) {
 				processResourceNotification(notification);
-			} else {
+			} else if (record) {
 				processObjectNotification(notification);
 			}
 		}
@@ -256,5 +298,4 @@ public class TransactionChangeRecorder
 			throw ise;
 		}
 	}
-
 }

org.eclipse.emf.transaction.tests/src/org/eclipse/emf/transaction/tests/AbstractTest.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: AbstractTest.java,v 1.3 2006/01/30 19:47:50 cdamus Exp $
+ * $Id: AbstractTest.java,v 1.4 2006/04/21 14:59:17 cdamus Exp $
  */
 package org.eclipse.emf.transaction.tests;
 
@@ -157,13 +157,7 @@ public class AbstractTest
 		
 		root = null;
 		if (testResource != null) {
-			if (testResource.isLoaded()) {
-				testResource.unload();
-			}
-			
-			if (testResource.getResourceSet() != null) {
-				testResource.getResourceSet().getResources().remove(testResource);
-			}
+			unloadAndRemove(testResource);
 			testResource = null;
 		}
 		
@@ -205,6 +199,16 @@ public class AbstractTest
 		}
 		
 		return result;
+	}
+	
+	protected void unloadAndRemove(Resource res) {
+		if (res.isLoaded()) {
+			res.unload();
+		}
+		
+		if (res.getResourceSet() != null) {
+			res.getResourceSet().getResources().remove(res);
+		}
 	}
 	
 	/**
@@ -319,7 +323,7 @@ public class AbstractTest
 	 * @param object the object
 	 * @return its name
 	 */
-	private String getName(EObject object) {
+	protected String getName(EObject object) {
 		return (String) GetName.INSTANCE.doSwitch(object);
 	}
 	

org.eclipse.emf.transaction.tests/src/org/eclipse/emf/transaction/tests/AllTests.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: AllTests.java,v 1.5 2006/04/12 22:09:38 cdamus Exp $
+ * $Id: AllTests.java,v 1.6 2006/04/21 14:59:17 cdamus Exp $
  */
 package org.eclipse.emf.transaction.tests;
 
@@ -49,6 +49,7 @@ public class AllTests
 		suite.addTest(ResourceSetListenersTest.suite());
 		suite.addTest(NotificationFilterTest.suite());
 		suite.addTest(TransactionOptionsTest.suite());
+		suite.addTest(TransactionChangeRecorderTest.suite());
 		suite.addTest(UndoRedoTest.suite());
 		suite.addTest(EditingDomainRegistryTest.suite());
 		suite.addTest(AbstractMultithreadTest.suite());

org.eclipse.emf.transaction.tests/src/org/eclipse/emf/transaction/tests/PerformanceTest.java
@@ -12,12 +12,30 @@
  *
  * </copyright>
  *
- * $Id: PerformanceTest.java,v 1.2 2006/04/03 19:10:59 cdamus Exp $
+ * $Id: PerformanceTest.java,v 1.3 2006/04/21 14:59:17 cdamus Exp $
  */
 package org.eclipse.emf.transaction.tests;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CompoundCommand;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.examples.extlibrary.Book;
+import org.eclipse.emf.examples.extlibrary.BookCategory;
+import org.eclipse.emf.examples.extlibrary.EXTLibraryFactory;
+import org.eclipse.emf.examples.extlibrary.Library;
+import org.eclipse.emf.transaction.ResourceSetChangeEvent;
+import org.eclipse.emf.transaction.ResourceSetListener;
+import org.eclipse.emf.transaction.ResourceSetListenerImpl;
+import org.eclipse.emf.transaction.RollbackException;
+import org.eclipse.emf.transaction.Transaction;
+import org.eclipse.emf.transaction.tests.fixtures.TestCommand;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -35,6 +53,8 @@ public class PerformanceTest extends AbstractTest {
 	private List timings;
 	private long clock;
 	
+	private int count = RUNS + OUTLIERS;
+	
 	public PerformanceTest(String name) {
 		super(name);
 	}
@@ -49,11 +69,6 @@ public class PerformanceTest extends AbstractTest {
 	 * <code>ReadWriteValidator.findTree(Transaction)</code> method.
 	 */
 	public void test_ReadWriteValidator_findTree_132590() {
-		int count = RUNS + OUTLIERS;
-		
-		System.out.println("Performance test: " + getName()); //$NON-NLS-1$
-		System.out.println("==============================="); //$NON-NLS-1$
-		
 		for (int i = 0; i < count; i++) {
 			startClock();
 			
@@ -63,13 +78,98 @@ public class PerformanceTest extends AbstractTest {
 			
 			System.out.println("Raw timing: " + timing); //$NON-NLS-1$
 		}
+	}
+	
+	/**
+	 * Measures the time taken to load a largish resource, to measure the
+	 * performance benefit of not recording resource load/unload events.
+	 */
+	public void test_loadLargishModelWithoutRecording() {
+		addStuffToTestResourceAndClose();
+
+		final Transaction[] originalTransaction = new Transaction[1];
+		final int[] precommitEvents = new int[1];
+		final int[] postcommitEvents = new int[1];
 		
-		removeOutliers();
+		// add a listener to incur the cost of broadcasting pre- and post-commit
+		ResourceSetListener listener = new ResourceSetListenerImpl() {
+			public Command transactionAboutToCommit(ResourceSetChangeEvent event)
+					throws RollbackException {
+				CompoundCommand result = null;
+				
+				if (originalTransaction[0] == null) {
+					originalTransaction[0] = event.getTransaction();
+				}
+				
+				if (event.getTransaction() == originalTransaction[0]) {
+					// only create a command on the first transaction
+					result = new CompoundCommand();
+					
+					for (Iterator iter = event.getNotifications().iterator(); iter.hasNext();) {
+						iter.next();
+						
+						precommitEvents[0]++;
+						
+						result.append(new TestCommand() {
+							public void execute() {}});
+					}
+				}
+				
+				return result;
+			}
 		
-		System.out.println("Mean time for " + RUNS + " runs: " + meanTiming()); //$NON-NLS-1$ //$NON-NLS-2$
-		System.out.println("Standard deviation: " + stddevTiming()); //$NON-NLS-1$
+			public void resourceSetChanged(ResourceSetChangeEvent event) {
+				for (Iterator iter = event.getNotifications().iterator(); iter.hasNext();) { 
+					iter.next();
+					
+					// do a trivial amount of work
+					postcommitEvents[0]++;
+					assertNotNull(event.getTransaction());
+				}
+			}};
+
+		domain.addResourceSetListener(listener);
 		
-		System.out.println("==============================="); //$NON-NLS-1$
+		Map options = new java.util.HashMap();
+//		options.put(Transaction.OPTION_NO_NOTIFICATIONS, Boolean.TRUE);
+//		options.put(Transaction.OPTION_NO_TRIGGERS, Boolean.TRUE);
+//		options.put(Transaction.OPTION_NO_VALIDATION, Boolean.TRUE);
+		
+		try {
+			for (int i = 0; i < count; i++) {
+				originalTransaction[0] = null;
+				
+				// we will report the number of events from just a single run
+				precommitEvents[0] = 0;
+				postcommitEvents[0] = 0;
+				
+				startClock();
+				
+				startWriting(options);
+				
+				testResource.load(Collections.EMPTY_MAP);
+				
+				commit();
+				
+				startWriting(options);
+				
+				testResource.unload();
+				
+				commit();
+				
+				long timing = stopClock();
+				
+				System.out.println("Raw timing: " + timing); //$NON-NLS-1$
+			}
+			
+			System.out.println("Number of pre-commit events : " + precommitEvents[0]); //$NON-NLS-1$
+			System.out.println("Number of post-commit events: " + postcommitEvents[0]); //$NON-NLS-1$
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Failed to load test resource: " + e.getLocalizedMessage()); //$NON-NLS-1$
+		} finally {
+			domain.removeResourceSetListener(listener);
+		}
 	}
 	
 	//
@@ -79,10 +179,20 @@ public class PerformanceTest extends AbstractTest {
 	protected void doSetUp() throws Exception {
 		super.doSetUp();
 		
+		System.out.println("Performance test: " + getName()); //$NON-NLS-1$
+		System.out.println("==============================="); //$NON-NLS-1$
+		
 		timings = new java.util.ArrayList();
 	}
 	
 	protected void doTearDown() throws Exception {
+		removeOutliers();
+		
+		System.out.println("Mean time for " + RUNS + " runs: " + meanTiming()); //$NON-NLS-1$ //$NON-NLS-2$
+		System.out.println("Standard deviation: " + stddevTiming()); //$NON-NLS-1$
+		
+		System.out.println("==============================="); //$NON-NLS-1$
+		
 		timings = null;
 		
 		super.doTearDown();
@@ -106,6 +216,60 @@ public class PerformanceTest extends AbstractTest {
 			
 			commit();
 		}
+	}
+	
+	/**
+	 * Adds a largish number of libraries to the test resource, each with
+	 * some books.
+	 */
+	protected void addStuffToTestResourceAndClose() {
+		startWriting();
+		
+		Date today = new Date();
+		
+		for (int i = 0; i < 50; i++) {
+			addLibraryWithBooks(today);
+		}
+		
+		commit();
+		
+		startReading();
+		
+		try {
+			testResource.save(Collections.EMPTY_MAP);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Failed to save test resource: " + e.getLocalizedMessage()); //$NON-NLS-1$
+		}
+		
+		testResource.unload();
+		
+		commit();
+	}
+	
+	protected void addLibraryWithBooks(Date today) {
+		Library lib = EXTLibraryFactory.eINSTANCE.createLibrary();
+		EList branches = lib.getBranches();
+		
+		for (int j = 0; j < 20; j++) {
+			Library branch = EXTLibraryFactory.eINSTANCE.createLibrary();
+			EList books = branch.getBooks();
+			
+			for (int i = 0; i < 10; i++) {
+				Book book = EXTLibraryFactory.eINSTANCE.createBook();
+	
+				book.setTitle("Foo"); //$NON-NLS-1$
+				book.setPages(30);
+				book.setCategory(BookCategory.BIOGRAPHY_LITERAL);
+				book.setCopies(7);
+				book.setPublicationDate(today);
+				books.add(book);
+			}
+			
+			branches.add(branch);
+		}
+		
+		testResource.getContents().add(lib);
 	}
 	
 	final void startClock() {

org.eclipse.emf.transaction.tests/src/org/eclipse/emf/transaction/tests/TransactionChangeRecorderTest.java
@@ -1,0 +1,329 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: TransactionChangeRecorderTest.java,v 1.1 2006/04/21 14:59:17 cdamus Exp $
+ */
+package org.eclipse.emf.transaction.tests;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.InternalEList;
+import org.eclipse.emf.transaction.impl.TransactionChangeRecorder;
+import org.eclipse.emf.transaction.util.TransactionUtil;
+
+
+/**
+ * Tests the <code>TransactionChangeRecorder</code> class, specifically.
+ *
+ * @author Christian W. Damus (cdamus)
+ */
+public class TransactionChangeRecorderTest extends AbstractTest {
+	
+	private Resource rootResource;
+	private Resource nestedResource1;
+	private Resource nestedResource2;
+	
+	public TransactionChangeRecorderTest(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		return new TestSuite(TransactionChangeRecorderTest.class, "Change Recorder Tests"); //$NON-NLS-1$
+	}
+
+	/**
+	 * Tests that the change recorder did not cause the nested resources to load
+	 * when loading in a read-only transaction (not creating change descriptions).
+	 */
+	public void test_nestedNotLoaded_readOnlyTX() {
+		startReading();
+		loadRoot();
+		
+		assertTrue(rootResource.isLoaded());
+		
+		assertFalse(nestedResource1.isLoaded());
+		assertFalse(nestedResource2.isLoaded());
+	}
+	
+	/**
+	 * Tests that the change recorder is correctly propagated to the objects in
+	 * a resource when it is loaded when loading in a read-only transaction
+	 * (not creating change descriptions).
+	 */
+	public void test_changeRecorderPropagatedOnLoad_readOnlyTX() {
+		startReading();
+		loadRoot();
+		
+		TransactionChangeRecorder recorder = getRecorder(rootResource);
+		
+		EPackage pkg = findPackage("root/nested1", true); //$NON-NLS-1$
+		assertSame(recorder, getRecorder(pkg));
+		
+		EList properContents = TransactionUtil.getProperContents(pkg);
+		assertEquals(1, properContents.size());
+		assertSame(EcorePackage.Literals.ECLASS, ((EObject) properContents.get(0)).eClass());
+		assertSame(recorder, getRecorder((Notifier) properContents.get(0)));
+		
+		InternalEList contents = (InternalEList) pkg.eContents();
+		assertEquals(2, contents.size());
+		
+		// check that the EPackage is not yet resolved
+		EObject obj = (EObject) contents.basicGet(0);
+		assertTrue(!(obj instanceof EPackage) || obj.eIsProxy());
+		obj = (EObject) contents.basicGet(1);
+		assertTrue(!(obj instanceof EPackage) || obj.eIsProxy());
+		
+		assertFalse(nestedResource1.isLoaded());
+		assertFalse(nestedResource2.isLoaded());
+		
+		// force load of the first nested resource by looking for its package
+		pkg = findPackage("root/nested1/nested2", true); //$NON-NLS-1$
+		assertFalse(pkg.eIsProxy());
+		assertSame(recorder, getRecorder(pkg));
+		
+		assertTrue(nestedResource1.isLoaded());
+		assertFalse(nestedResource2.isLoaded());
+	}
+
+	/**
+	 * Tests that the <code>setTarget()</code> method does not cause proxy
+	 * resolution.
+	 */
+	public void test_propagation_setTarget() {
+		startReading();
+		
+		TransactionChangeRecorder recorder = getRecorder(rootResource);
+		rootResource.eAdapters().remove(recorder);
+		
+		loadRoot();
+		
+		commit();
+		
+		startWriting();
+		
+		assertFalse(nestedResource1.isLoaded());
+		assertFalse(nestedResource2.isLoaded());
+		
+		rootResource.eAdapters().add(recorder);
+		
+		assertFalse(nestedResource1.isLoaded());
+		assertFalse(nestedResource2.isLoaded());
+		
+		// our editing domain doesn't know that this resource was loaded, so
+		//   we have to unload it behind its back also, otherwise tearDown()
+		//   will throw IllegalStateException
+		rootResource.eAdapters().add(recorder);
+		unloadAndRemove(rootResource);
+		
+		commit();
+	}
+
+	/**
+	 * Tests that the change recorder did not cause the nested resources to load
+	 * when loading in a read-write transaction (creating change descriptions).
+	 */
+	public void test_nestedNotLoaded_writeTX() {
+		startWriting();
+		loadRoot();
+		
+		assertTrue(rootResource.isLoaded());
+		
+		assertFalse(nestedResource1.isLoaded());
+		assertFalse(nestedResource2.isLoaded());
+	}
+	
+	/**
+	 * Tests that the change recorder is correctly propagated to the objects in
+	 * a resource when it is loadedwhen loading in a read-write transaction
+	 * (creating change descriptions).
+	 */
+	public void test_changeRecorderPropagatedOnLoad_writeTX() {
+		startWriting();
+		loadRoot();
+		
+		TransactionChangeRecorder recorder = getRecorder(rootResource);
+		
+		EPackage pkg = findPackage("root/nested1", true); //$NON-NLS-1$
+		assertSame(recorder, getRecorder(pkg));
+		
+		EList properContents = TransactionUtil.getProperContents(pkg);
+		assertEquals(1, properContents.size());
+		assertSame(EcorePackage.Literals.ECLASS, ((EObject) properContents.get(0)).eClass());
+		assertSame(recorder, getRecorder((Notifier) properContents.get(0)));
+		
+		InternalEList contents = (InternalEList) pkg.eContents();
+		assertEquals(2, contents.size());
+		
+		// check that the EPackage is not yet resolved
+		EObject obj = (EObject) contents.basicGet(0);
+		assertTrue(!(obj instanceof EPackage) || obj.eIsProxy());
+		obj = (EObject) contents.basicGet(1);
+		assertTrue(!(obj instanceof EPackage) || obj.eIsProxy());
+		
+		assertFalse(nestedResource1.isLoaded());
+		assertFalse(nestedResource2.isLoaded());
+		
+		// force load of the first nested resource by looking for its package
+		pkg = findPackage("root/nested1/nested2", true); //$NON-NLS-1$
+		assertFalse(pkg.eIsProxy());
+		assertSame(recorder, getRecorder(pkg));
+		
+		assertTrue(nestedResource1.isLoaded());
+		assertFalse(nestedResource2.isLoaded());
+	}
+	
+	//
+	// Framework methods
+	//
+	
+	protected void doSetUp() throws Exception {
+		super.doSetUp();
+		
+		ResourceSet rset = domain.getResourceSet();
+		
+		try {
+			rootResource = rset.getResource(
+					URI.createURI(EmfTransactionTestsBundle.getEntry(
+						"/test_models/test_model.ecore").toString()), //$NON-NLS-1$
+						true);
+			rootResource.setURI(URI.createPlatformResourceURI(
+					"/" + PROJECT_NAME + "/test_model.ecore")); //$NON-NLS-1$ //$NON-NLS-2$
+			
+			nestedResource1 = rset.createResource(URI.createPlatformResourceURI(
+					"/" + PROJECT_NAME + "/test_model1.ecore")); //$NON-NLS-1$ //$NON-NLS-2$
+			nestedResource2 = rset.createResource(URI.createPlatformResourceURI(
+					"/" + PROJECT_NAME + "/test_model2.ecore")); //$NON-NLS-1$ //$NON-NLS-2$
+			
+			startWriting();
+			
+			EPackage pkg = findPackage("root/nested1/nested2", true); //$NON-NLS-1$
+			nestedResource1.getContents().add(pkg);  // cross-resource-contained
+			
+			pkg = findPackage("root/nested1/nested2/nested3/nested4", true); //$NON-NLS-1$
+			nestedResource2.getContents().add(pkg);  // cross-resource-contained
+			
+			commit();
+			
+			startReading();
+			
+			// save the units
+			rootResource.save(Collections.EMPTY_MAP);
+			nestedResource1.save(Collections.EMPTY_MAP);
+			nestedResource2.save(Collections.EMPTY_MAP);
+			
+			// unload them
+			rootResource.unload();
+			nestedResource1.unload();
+			nestedResource2.unload();
+			
+			commit();
+		} catch (IOException e) {
+			fail("Failed to create test model: " + e.getLocalizedMessage()); //$NON-NLS-1$
+		}
+	}
+	
+	protected void doTearDown() throws Exception {
+		if (rootResource != null) {
+			unloadAndRemove(rootResource);
+			rootResource = null;
+		}
+		
+		if (nestedResource1 != null) {
+			unloadAndRemove(nestedResource1);
+			nestedResource1 = null;
+		}
+		
+		if (nestedResource2 != null) {
+			unloadAndRemove(nestedResource2);
+			nestedResource2 = null;
+		}
+		
+		super.doTearDown();
+	}
+	
+	protected EPackage findPackage(String qname, boolean require) {
+		EPackage result = (EPackage) find(rootResource, qname);
+		
+		if (require) {
+			assertNotNull("Did not find package " + qname, result); //$NON-NLS-1$
+		}
+		
+		return result;
+	}
+	
+	protected EClass findClass(String qname, boolean require) {
+		EClass result = (EClass) find(rootResource, qname);
+		
+		if (require) {
+			assertNotNull("Did not find class " + qname, result); //$NON-NLS-1$
+		}
+		
+		return result;
+	}
+
+	/**
+	 * Gets the name of an Ecore object.
+	 * 
+	 * @param object the object
+	 * @return its name
+	 */
+	protected String getName(EObject object) {
+		if (object instanceof ENamedElement) {
+			return ((ENamedElement) object).getName();
+		}
+		
+		return super.getName(object);
+	}
+	
+	protected void loadRoot() {
+		try {
+			rootResource.load(Collections.EMPTY_MAP);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Failed to load root resource: " + e.getLocalizedMessage()); //$NON-NLS-1$
+		}
+	}
+	
+	protected TransactionChangeRecorder getRecorder(Notifier notifier) {
+		TransactionChangeRecorder result = null;
+		
+		for (Iterator iter = notifier.eAdapters().iterator(); iter.hasNext();) {
+			Object next = iter.next();
+			
+			if (next instanceof TransactionChangeRecorder) {
+				result = (TransactionChangeRecorder) next;
+				break;
+			}
+		}
+		
+		assertNotNull("Did not find change recorder", result); //$NON-NLS-1$
+		
+		return result;
+	}
+}

org.eclipse.emf.transaction.tests/src/org/eclipse/emf/transaction/util/tests/TransactionUtilTests.java
@@ -12,13 +12,20 @@
  *
  * </copyright>
  *
- * $Id: TransactionUtilTests.java,v 1.1 2006/01/30 21:40:51 cdamus Exp $
+ * $Id: TransactionUtilTests.java,v 1.2 2006/04/21 14:59:17 cdamus Exp $
  */
 package org.eclipse.emf.transaction.util.tests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.edit.domain.EditingDomain;
@@ -87,5 +94,57 @@ public class TransactionUtilTests extends AbstractTest {
 			}};
 			
 		assertSame(domain, TransactionUtil.getEditingDomain((Object) edp));
+	}
+	
+	/**
+	 * Tests the <code>getProperContents(EObject)</code> method.
+	 */
+	public void test_getProperContents() {
+		EPackage rootPackage = EcoreFactory.eINSTANCE.createEPackage();
+		rootPackage.setName("root"); //$NON-NLS-1$
+		
+		// proper contents consistent with EContents when no cross-resource
+		//   containment is involved
+		
+		EList contents = rootPackage.eContents();
+		EList properContents = TransactionUtil.getProperContents(rootPackage);
+		
+		EClass eclass = EcoreFactory.eINSTANCE.createEClass();
+		eclass.setName("Foo"); //$NON-NLS-1$
+		rootPackage.getEClassifiers().add(eclass);
+		
+		assertTrue(contents.contains(eclass));
+		assertTrue(properContents.equals(contents));
+		
+		EPackage nested = EcoreFactory.eINSTANCE.createEPackage();
+		nested.setName("nested"); //$NON-NLS-1$
+		rootPackage.getESubpackages().add(nested);
+		
+		assertTrue(contents.contains(eclass));
+		assertTrue(contents.contains(nested));
+		assertTrue(properContents.equals(contents));
+		
+		ResourceSet rset = new ResourceSetImpl();
+		Resource res1 = rset.createResource(URI.createURI("null:///res1.ecore")); //$NON-NLS-1$
+		Resource res2 = rset.createResource(URI.createURI("null:///res2.ecore")); //$NON-NLS-1$
+		
+		res1.getContents().add(rootPackage);
+		res2.getContents().add(nested);  // cross-resource-contained
+		
+		// the nested package is no longer properly contained
+		
+		assertTrue(contents.contains(nested));
+		assertFalse(properContents.contains(nested));
+		assertFalse(properContents.equals(contents));
+		assertTrue(properContents.contains(eclass));
+		
+		res2.unload();
+		
+		assertTrue(nested.eIsProxy());
+		
+		assertTrue(contents.contains(nested));
+		assertFalse(properContents.contains(nested));
+		assertFalse(properContents.equals(contents));
+		assertTrue(properContents.contains(eclass));
 	}
 }

org.eclipse.emf.transaction.tests/test_models/test_model.ecore
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ecore:EPackage xmi:version="2.0"
+    xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:ecore="http://www.eclipse.org/emf/2002/Ecore" name="root">
+  <eAnnotations source="http:///org.eclipse.emf.transaction.tests" references="#//nested1/nested2"/>
+  <eClassifiers xsi:type="ecore:EClass" name="A"/>
+  <eSubpackages name="nested1">
+    <eClassifiers xsi:type="ecore:EClass" name="B"/>
+    <eSubpackages name="nested2">
+      <eClassifiers xsi:type="ecore:EClass" name="C"/>
+      <eSubpackages name="nested3" nsPrefix="">
+        <eClassifiers xsi:type="ecore:EClass" name="D"/>
+        <eSubpackages name="nested4">
+          <eClassifiers xsi:type="ecore:EClass" name="E"/>
+        </eSubpackages>
+      </eSubpackages>
+    </eSubpackages>
+  </eSubpackages>
+</ecore:EPackage>

org.eclipse.emf.transaction/src/org/eclipse/emf/transaction/ReadFilter.java
@@ -12,12 +12,13 @@
  *
  * </copyright>
  *
- * $Id: ReadFilter.java,v 1.3 2006/01/06 16:16:22 cdamus Exp $
+ * $Id: ReadFilter.java,v 1.4 2006/04/21 14:59:07 cdamus Exp $
  */
 package org.eclipse.emf.transaction;
 
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.transaction.impl.ResourceSetManager;
 
@@ -60,8 +61,27 @@ class ReadFilter extends NotificationFilter {
 				return checkResource(notification);
 			}
 			
-			return (!(notifier instanceof EObject)
-					|| isLoadingOrUnloading(((EObject) notifier).eResource()));
+			if (notifier instanceof EObject) {
+				EObject eobject = (EObject) notifier;
+				
+				// ignore changes to a container feature because these will
+				//    always be accomanied by changes in the opposite
+				//    containment feature (because a container is defined by
+				//    being the opposite of a containment).  A special case is
+				//    resolution of a containment proxy, in which case the
+				//    eInverseAdd() will notify on the container feature but
+				//    we do not get a containment notification; this is a
+				//    read-compatible change
+				Object feature = notification.getFeature();
+				if ((feature instanceof EReference)
+						&& ((EReference) feature).isContainer()) {
+					return true;
+				}
+				
+				return isLoadingOrUnloading(eobject.eResource());
+			}
+			
+			return true;  // not an EObject
 		default:
 			// any other event type is not one that is supported by EMF, so
 			//    we will not prevent it

commit 03eed78097c7e29b80d91f55a4fbf29b38f8900d

[504494] Make sure ValidateEditSupport is called on Notification.MOVE
The TransactionChangeRecorder normally invokes the configured
ValidateEditSupport (if any) on each model change, but changes of type
Notification.MOVE were not considered.

The bug, which looks like a simple oversight, was apparently
introduced by commit e0ab1c36a for bug #137503.

Bug: 504494
Change-Id: I2b50770329a88da49180fbb3fa2287218f38cd0e
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

org.eclipse.emf.transaction/src/org/eclipse/emf/transaction/impl/TransactionChangeRecorder.java
@@ -190,6 +190,7 @@ public class TransactionChangeRecorder extends ChangeRecorder {
 		case Notification.ADD_MANY:
 		case Notification.REMOVE:
 		case Notification.REMOVE_MANY:
+		case Notification.MOVE:
 			Resource sourceRes = null;
 			Object notifier = notification.getNotifier();
 			